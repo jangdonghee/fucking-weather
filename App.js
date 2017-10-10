b38db7e4c4988fae08dfd6501bd33c1d86a3379b
jangdonghee <lbghini@mgail.com>
2017-10-10 12:41:52 +0900

    loading

--- App.js
+++ App.js
@@ -2,10 +2,24 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = ({
+      isLoaded: false
+    });
+  }
   render() {
+
+    const {isLoaded} = this.state;
+
     return (
       <View style={styles.container}>
-        <Text>Hello I`m Native Developer Now</Text>
+        {isLoaded ? null : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting the fucking weather</Text>
+          </View>
+        )}
       </View>
     );
   }
@@ -14,8 +28,17 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
+  loading: {
+    flex:1, 
+    backgroundColor: '#FDF6AA',
+    justifyContent: 'flex-end',
+    paddingLeft: 25
+
+  },
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 100
+  }
 });
